dbcf9c702f1e11ea962e2764a365fb50815733d9
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2021-11-29 17:01:55 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,64 @@
+/*
+let a = 10
+let b = 10
+console.log(b)
+// Será exibido o numero 10//
+*/
+
+/*
+b = 5
+console.log(a,b)
+// Serão exibidos os numeros 10, 5//
+*/
+
+/*
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+// Serão exibidos as letras b, c, a//
+*/
+
+/*
+let p = prompt("Quantas horas você trabalha por dia")
+let t = prompt("Quanto você recebe por dia")
+alert(`Você recebe ${t/p} por hora`)*/
+
+/* Eu colocaria a nomenclatura assim:
+
+let horasPorDia = prompt("Quantas horas você trabalha ")
+let valorPorDia = prompt("Quanto você recebe por dia")
+alert(`Voce recebe ${valorPorDia/horasPorDia} por hora`) */
+
+
+/*
+let nome = prompt("Digite seu nome?");
+let idade = prompt("Qual a sua idade");
+console.log(typeof nome);
+console.log(typeof idade);
+Foi impresso um tipo de Strings
+*/
+
+/*
+let nome =  prompt("Digite seu nome?");
+let idade = prompt("Qual a sua idade");
+console.log("Olá", nome, "você tem", idade, "anos");
+*/
+
+const pergunta1 = ("Você tem celular?");
+const pergunta2 = ("Você tem carro?");
+const pergunta3 = ("Você tem cachorro?");
+let resposta1 = Sim;
+let resposta2 = Não;
+let resposta3 = Talveis;
+console.log("1 - ", pergunta1, "-", resposta1);
+
+
+
+
+
+
+
+

757fead3839947fc5cd4eb831470208aca1bf5ca
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2021-12-02 16:42:45 -0300

    Exercicios incompletos

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,74 @@
+//INTERPRETAÇÃO
+//Exercicio 1
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))  */
+//a) Está multiplicando 2x5 e 10x5. Será impresso 10 e 50.
+/*
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+(minhaFuncao(2)) (minhaFuncao(10))
+//b) informa que não é uma função. */
+
+//Exercicio 2
+/*
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) */
+//a) Depois de inserido o texto, achei que seria transformado em letras minusculas,porem só está informando True caso tenha a palavra cenoura no texto, caso não tenha cenoura no texto, será False.
+/*
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta) 
+*/
+//b)
+// i.  Eu gosto de cenoura - True
+//ii.  CENOURA é bom pra vista - True
+//iii. Cenouras crescem na terra - True
+
+
+//ESCRITA DE CODIGO
+//Exercicio 1
+//a)
+
+function mensagemSobreMim() {
+return textoSMim = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+}
+
+console.log(mensagemSobreMim())
+
+ 
+ function mudancaDeParam (nome, idade, cidade, souEstudante) {
+    
+    //nome = "Anderson"
+    //idade = 38
+    //cidade = "Curitiba"
+    //souEstudande = "Labenu"
+ 
+console.log(`Eu sou o ${nome}, tenho ${idade}, moro em ${cidade} e sou ${souEstudante}.`)
+ }
+mudancaDeParam("Anderson", "38","Curitiba", "Labenu")
+
+//c)
+
+function parametro4()
+
+
+
+

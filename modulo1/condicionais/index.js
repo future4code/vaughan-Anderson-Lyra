4de028545d6b21f6e1ed7a17feafb767bc7811ff
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2021-12-07 16:55:18 -0300

    Exercicio de Condicionais incompletos

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,41 @@
+//EXERCICIO INTERPRETAÇÃO
+//1)
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if (numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+
+//a) Ele pede para o usuário digitar um numero e o resultado será
+//   será exibido "Passou no teste" se ele digitar numero par e a outro
+//   mensagem será exibida se o numero for impar.
+
+//b) Para numeros Pares.
+//c) Para numeros Impares.
+
+//2)
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+
+//a)

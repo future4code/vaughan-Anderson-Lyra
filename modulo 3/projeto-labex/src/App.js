1ab3d5da0b730e11a630a09f0ddc5e8d90396c1c
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-02-14 19:58:30 -0300

    Projeto-labex dia 1

---
+++ modulo 3/projeto-labex/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div >
+      <p> LabeX</p>
+      <button>Ver Viagens</button>
+      <button>Área de Admin</button>
+    </div>
+  );
+}
+
+export default App;

3439a8a66cdbf4ac4506785562e641669f1215b8
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-03-22 17:00:33 -0300

    Exercicio de NODE

---
+++ BackEnd/Modulo 1/node-package-json/template-node-npm/Exercicio_1/index.js
@@ -0,0 +1,11 @@
+
+// a) node "nome do arquivo"
+
+//b)
+const nome = "Anderson"
+const idade = Number("39")
+console.log(`Olá, ${nome}! Vecê tem ${idade} anos.`)
+
+//c)
+const idadeSete = Number("7") + idade
+console.log(`Olá, ${nome}! Vecê tem ${idade} anos. Em sete anos você terá ${idadeSete}`)
---
+++ BackEnd/Modulo 1/node-package-json/template-node-npm/Exercicio_2/index.js
@@ -0,0 +1,14 @@
+
+
+const numero1 = Number(process.argv[2])
+const numero2 = Number(process.argv[3])
+
+
+console.log("Soma: ", numero1+numero2)
+console.log("Subt: ", numero1-numero2)
+console.log("Div: ", numero1/numero2)
+console.log("Mult: ", numero1*numero2)
+
+
+
+

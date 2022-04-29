5b1b8982386e1437570c0207ce012c3a20b07205
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-04-29 20:45:06 -0300

    Projeto Cookenu

--- BackEnd/Modulo 1/Cookenu/src/app.ts
+++ BackEnd/Modulo 1/Cookenu/src/app.ts
@@ -1,5 +1,4 @@
 import express, { Express } from "express";
-import knex from "knex";
 import cors from "cors";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
@@ -8,26 +7,15 @@ import { AddressInfo } from "net";
 dotenv.config();
 
 
-export const connection = knex({
- client: "mysql",
- connection: {
- host: process.env.DB_HOST,
- port: 3306,
- user: process.env.DB_USER,
- password: process.env.DB_PASSWORD,
- database: process.env.DB_SCHEMA,
-},
-});
-
-const app: Express = express();
+export const app: Express = express();
 app.use(express.json());
 app.use(cors());
 const server = app.listen(process.env.PORT || 3003, () => {
-if (server) {
- const address = server.address() as AddressInfo;
- console.log(`Server is running in http://localhost:
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:
 ${address.port}`);
-} else {
- console.error(`Failure upon starting server.`);
-}
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
 });
--- BackEnd/Modulo 1/Cookenu/src/data/UserDatabase.ts
+++ BackEnd/Modulo 1/Cookenu/src/data/UserDatabase.ts
@@ -2,53 +2,71 @@ import { BaseDatabase } from "./BaseDatabase";
 import { User } from "../entities/User";
 
 
+
 export class UserDatabase extends BaseDatabase {
     public async createUser(user: User) {
 
-           try {
+        try {
             await BaseDatabase.connection("Cookenu")
-            .insert({
-                id: user.getId(),
-                name: user.getName(),
-                email: user.getEmail(),
-                password: user.getPassword(),
-                role: user.getRole()
-            });
-        } catch (error:any) {
+                .insert({
+                    id: user.getId(),
+                    name: user.getName(),
+                    email: user.getEmail(),
+                    password: user.getPassword(),
+                    role: user.getRole()
+                });
+        } catch (error: any) {
             throw new Error(error.sqlMessage || error.message);
-        }               
-    
         }
-      
-public async findUserByEmail(email: string):Promise <User> {
-    try {
-        const user = await BaseDatabase.connection("Cookenu")
-        .select("*")
-        .where ({email});
-        return user[0] && User.toUserModel(user[0]);
-        
-    } catch (error:any) {
-        throw new Error(error.sqlMessage || error.message);
+
+    }
+
+
+
+    public async findUserByEmail(email: string): Promise<User> {
+        try {
+            const user = await BaseDatabase.connection("Cookenu")
+                .select("*")
+                .where({ email });
+            return user[0] && User.toUserModel(user[0]);
+
+        } catch (error: any) {
+            throw new Error(error.sqlMessage || error.message);
+        }
+    }
+
+    public async getUserById(id: string): Promise<User> {
+        try {
+            const user = await BaseDatabase.connection("Cookenu")
+                .select("*")
+                .where({ id });
+            return user[0] && User.toUserModel(user[0]);
+
+        }
+        catch (error: any) {
+            throw new Error(error.sqlMessage || error.message);
+        }
+    }
+
+
+    public async getAllUsers(): Promise<User[]> {
+        try {
+
+            const users = await BaseDatabase.connection("Cookenu")
+                .select(
+                    "id",
+                    "name",
+                    "email",
+                    "role"
+                );
+            return users.map(user => User.toUserModel(user));
+        } catch (error: any) {
+            throw new Error(error.sqlMessage || error.message);
+        }
     }
 }
 
 
-public async getAllUsers(): Promise<User[]> {
-    try {
-       
-    const users = await BaseDatabase.connection("Cookenu")
-    .select(
-        "id",
-        "name",
-        "email",
-        "role"
-    );
-    return users.map(user => User.toUserModel(user));
-    } catch (error:any) {
-        throw new Error(error.sqlMessage || error.message);
-    }
-}
-}
 
 
 

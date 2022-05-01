b4cc2f4b892a4345895ce2c104b4ebebc174be8a
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-05-01 18:30:03 -0300

    Prejeto Cookenu

--- BackEnd/Modulo 1/Cookenu/src/endpoints/profileUserId.ts
+++ BackEnd/Modulo 1/Cookenu/src/endpoints/profileUserId.ts
@@ -2,16 +2,25 @@ import { Request, Response } from "express";
 import { UserDatabase } from "../data/UserDatabase";
 import { Authenticator } from "../Services/Authenticator";
 
+export const profileUserId  = async (req: Request, res: Response) => {
+    try { 
+        const id = req.params.id;
+        const userDataBase = new UserDatabase();
 
-export const getProfile = async (req: Request, res: Response) => {
-    try {
+        const user = await userDataBase.getUserById(id);
+
+        if (!user) {
+            throw new Error("Usuário não encontrado");
+        }
+
         const token = req.headers.authorization as string;
 
         const authenticator = new Authenticator();
         const authenticationData = authenticator.getTokenData(token);
 
-        const userDataBase = new UserDatabase();
-        const user = await userDataBase.getUserById(authenticationData.id);
+        if (!token || "string" !== typeof token) {
+            res.status(401).send({ message: "Não autorizado" });
+        }
 
         res.status(200).send({
             id: user.getId(),
@@ -22,7 +31,8 @@ export const getProfile = async (req: Request, res: Response) => {
 
     } catch (error: any) {
         res.status(400).send({
-            message: error.message
+            message: error.message || error.sqlMessage
         });
+
     }
 }

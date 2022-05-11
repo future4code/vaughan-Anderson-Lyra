e511e5f80dd1d92aaef14c99252e62f7bec9eed4
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-05-11 19:15:55 -0300

    Exercicio Testes-Backend

---
+++ BackEnd/Modulo 1/testes-backend/template-testes-backend/src/controller/UserController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import userBusiness from "../business/UserBusiness";
+import { CustomError } from "../errors/CustomError";
+
+export class UserController {
+
+   public async signup(req: Request, res: Response) {
+      try {
+         const { name, role, email, password } = req.body
+         const result = await userBusiness.signup(
+            name,
+            email,
+            password,
+            role
+         );
+         res.status(200).send(result);
+      } catch (error) {
+         if (error instanceof CustomError) {
+         const { statusCode, message } = error
+         res.status(statusCode || 400).send({ message });
+      }
+   }
+   }
+
+   public async login(req: Request, res: Response) {
+      try {
+         const { email, password } = req.body
+         const result = await userBusiness.login(email, password);
+         res.status(200).send(result);
+      } catch (error) {
+         if (error instanceof CustomError) {
+         const { statusCode, message } = error
+         res.status(statusCode || 400).send({ message });
+      }
+   }
+   }
+}
+
+export default new UserController()

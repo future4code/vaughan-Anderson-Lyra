b2e46b40d1b93128c61de508d3c593dfc1862279
Anderson-Felix-de-Lyra <felix_lyra@hotmail.com>
2022-05-16 18:28:24 -0300

    Case WireCard em construção

---
+++ BackEnd/Case WireCard/src/model/Clients.ts
@@ -0,0 +1,19 @@
+
+export class Client {
+    constructor(
+        private id: string,
+        private name: string
+    ){}
+
+    static clientModel(client: Client) {
+        return new Client(client.id, client.name)
+    }
+}
+
+export interface ClientInsert {
+     name: string
+}
+
+export interface ClientInputDTO extends ClientInsert {
+    id: string
+} 
---
+++ BackEnd/Case WireCard/src/model/Payment.ts
@@ -0,0 +1,44 @@
+export class Payment {
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private cpf: string,
+	    private amount: number,
+        private type: string,
+        private card_holder: string,
+        private card_number: string,
+        private card_expiration: string,
+        private card_cvv: string,
+        private id_client: string,
+        private id_buyer: string
+        ){}
+
+    static paymenttModel(payment: Payment) {
+        return new Payment(payment.id,payment.name, payment.email, payment.cpf, payment.amount, payment.type, 
+            payment.card_holder, payment.card_number, payment.card_expiration, payment.card_cvv, payment.id_client, payment.id_buyer)
+    }
+}
+
+export enum TYPE {
+    CARTAO = "Cartão de Crédito",
+    BOLETO = "Boleto Bancário"
+}
+
+
+export type PaymentInsert = {
+    name: string,
+    email: string,
+    cpf: string,
+    amount: number,
+    type: TYPE,
+    card_holder?: string,
+    card_number?: string ,
+    card_expiration?: string,
+    card_cvv?:  string,
+    id_client: string,
+}
+
+export interface PaymentInputDTO extends PaymentInsert{
+    id: string
+}
